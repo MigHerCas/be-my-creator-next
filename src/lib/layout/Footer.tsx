3069add3e40314837d310a61e57d83c0afb6e7d3
Miguel Hernanz <migherdcas@gmail.com>
2022-12-19 16:09:31 +0800

    remove footer content for fresh start

--- src/lib/layout/Footer.tsx
+++ src/lib/layout/Footer.tsx
@@ -1,16 +1,5 @@
-import { Flex, Link, Text } from "@chakra-ui/react";
-
 const Footer = () => {
-  return (
-    <Flex as="footer" width="full" justifyContent="center">
-      <Text fontSize="sm" color="gray.500">
-        {new Date().getFullYear()} -{" "}
-        <Link href="https://sznm.dev" isExternal rel="noopener noreferrer">
-          sznm.dev
-        </Link>
-      </Text>
-    </Flex>
-  );
+  return <div />;
 };
 
 export default Footer;
