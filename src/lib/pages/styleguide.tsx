d9bdb5e8f16e9dd61c8d502f5526d6c8bbeb15e7
Miguel Hernanz <migherdcas@gmail.com>
2022-12-20 22:04:03 +0800

    create Styleguide page

---
+++ src/lib/pages/styleguide.tsx
@@ -0,0 +1,46 @@
+import { Container, Heading, Text } from "@chakra-ui/react";
+
+const Styleguide = () => {
+  return (
+    <Container>
+      {/* Heading */}
+      <Heading size="xl">Be My Creator!</Heading>
+      <Heading size="lg">Be My Creator!</Heading>
+      <Heading size="md">Be My Creator!</Heading>
+      <Heading size="sm">Be My Creator!</Heading>
+      <Heading size="xs">Be My Creator!</Heading>
+
+      {/* Text */}
+      <Text size="lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. Duis cursus, mi quis viverra
+        ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+        Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut
+        sem vitae risus tristique posuere.
+      </Text>
+      <Text size="md">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. Duis cursus, mi quis viverra
+        ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+        Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut
+        sem vitae risus tristique posuere.
+      </Text>
+      <Text size="sm">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. Duis cursus, mi quis viverra
+        ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+        Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut
+        sem vitae risus tristique posuere.
+      </Text>
+      <Text size="xs">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. Duis cursus, mi quis viverra
+        ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+        Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut
+        sem vitae risus tristique posuere.
+      </Text>
+    </Container>
+  );
+};
+
+export default Styleguide;
---
+++ src/pages/styleguide.tsx
@@ -0,0 +1,3 @@
+import Styleguide from "lib/pages/styleguide";
+
+export default Styleguide;
