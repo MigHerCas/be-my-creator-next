ff9c31546f74c81a1e384bd70f975d44ac839e44
Miguel Hernanz <migherdcas@gmail.com>
2022-12-20 14:27:27 +0800

    render VideoPlayer component as children of Modal

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,23 +1,16 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
 
+import Modal from "lib/components/modal/Modal";
 import VideoPlayer from "lib/components/video/VideoPlayer";
 
 const Home = () => {
   return (
     <Flex>
       <NextSeo title="Home" />
-
-      <Box
-        w={220}
-        minH={380}
-        border="4px"
-        borderColor="white"
-        borderRadius="25"
-        overflow="hidden"
-      >
+      <Modal>
         <VideoPlayer />
-      </Box>
+      </Modal>
     </Flex>
   );
 };
