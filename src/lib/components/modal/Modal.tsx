ffe4060d95fac6750ec2c8d2d2268c364625bbd2
Miguel Hernanz <migherdcas@gmail.com>
2022-12-20 14:25:09 +0800

    create modal component

---
+++ src/lib/components/modal/Modal.tsx
@@ -0,0 +1,44 @@
+import { VisuallyHidden } from "@chakra-ui/react";
+import type { DialogProps } from "@reach/dialog";
+import { DialogContent, DialogOverlay } from "@reach/dialog";
+import type { FC } from "react";
+import { useState } from "react";
+
+// Styles
+import "@reach/dialog/styles.css";
+import styles from "./styles.module.css";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Modal: FC<Props> = ({ children }) => {
+  const [showDialog, setShowDialog] = useState(false);
+  const open = () => setShowDialog(true);
+  const close = () => setShowDialog(false);
+
+  const dialogOptions = {
+    isOpen: showDialog,
+    onDismiss: close,
+  } as DialogProps;
+
+  return (
+    <div>
+      <button type="button" onClick={open}>
+        Open Dialog
+      </button>
+
+      <DialogOverlay className={styles.overlay} {...dialogOptions}>
+        <DialogContent className={styles.content}>
+          <button type="button" className={styles.closeButton} onClick={close}>
+            <VisuallyHidden>Close</VisuallyHidden>
+            <span aria-hidden>×</span>
+          </button>
+          {children}
+        </DialogContent>
+      </DialogOverlay>
+    </div>
+  );
+};
+
+export default Modal;
