eb28f092f887d2cb7aa9690d5252d8e7ad12b9c3
Miguel Hernanz <migherdcas@gmail.com>
2022-12-20 16:35:07 +0800

    add breakpoints to extend theme object

--- src/lib/styles/theme/index.ts
+++ src/lib/styles/theme/index.ts
@@ -1,5 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 
+import { breakpoints } from "./breakpoints";
 import { colors } from "./colors";
 import { components } from "./components";
 import { config } from "./config";
@@ -10,6 +11,7 @@ const customTheme = extendTheme({
   colors,
   config,
   components,
+  breakpoints,
 });
 
 export default customTheme;
