1be590f8b40d29885220cb205a14d08d1eb7e762
Miguel Hernanz <migherdcas@gmail.com>
2022-12-20 22:42:53 +0800

    sampleText helper

---
+++ src/lib/helpers/text.ts
@@ -0,0 +1,5 @@
+export const sampleText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. Duis cursus, mi quis viverra
+        ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+        Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut
+        sem vitae risus tristique posuere.`;
