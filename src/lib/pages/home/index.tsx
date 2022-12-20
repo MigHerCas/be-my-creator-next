5b11846a4a7e016384ad03dc746b064551a75f52
Miguel Hernanz <migherdcas@gmail.com>
2022-12-20 13:37:15 +0800

    include new vanilla component and remove videojs one

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,42 +1,24 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
-import type { VideoJsPlayerOptions } from "video.js";
 
-import VideoPlayer from "lib/components/video";
+import VideoPlayer from "lib/components/video/VideoPlayer";
 
 const Home = () => {
-  const videoJsOptions: VideoJsPlayerOptions = {
-    autoplay: true,
-    muted: true,
-    loop: true,
-    controls: false,
-    preload: "auto",
-    bigPlayButton: false,
-    responsive: true,
-    fluid: true,
-    aspectRatio: "9:16",
-    poster: "/images/poster.jpg",
-    sources: [
-      {
-        src: "/videos/trend.mp4",
-        type: "video/mp4",
-      },
-    ],
-  };
-
   return (
-    <Box
-      w={220}
-      minH={380}
-      border="4px"
-      borderColor="white"
-      borderRadius="25"
-      overflow="hidden"
-    >
+    <Flex>
       <NextSeo title="Home" />
 
-      <VideoPlayer options={videoJsOptions} />
-    </Box>
+      <Box
+        w={220}
+        minH={380}
+        border="4px"
+        borderColor="white"
+        borderRadius="25"
+        overflow="hidden"
+      >
+        <VideoPlayer />
+      </Box>
+    </Flex>
   );
 };
 
