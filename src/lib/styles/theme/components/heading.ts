cce66251130fd5c5cedcd01be821b7e8170de98d
Miguel Hernanz <migherdcas@gmail.com>
2022-12-20 21:23:15 +0800

    complete heading configs

--- src/lib/styles/theme/components/heading.ts
+++ src/lib/styles/theme/components/heading.ts
@@ -2,24 +2,39 @@ import { defineStyleConfig } from "@chakra-ui/react";
 
 export const Heading = defineStyleConfig({
   // The styles all headings configs have in common
-  baseStyle: {},
+  baseStyle: {
+    marginTop: 0,
+  },
   sizes: {
-    extraLarge: {},
-    large: {},
-    medium: {},
-    regular: {},
-    light: {},
-    small: {},
-    titleTag: {},
+    xs: {
+      fontSize: "1.5em",
+      lineHeight: "1.4",
+      fontWeight: "600",
+    },
+    sm: {
+      fontSize: "1.8em",
+      lineHeight: "1.4",
+      fontWeight: "700",
+    },
+    md: {
+      fontSize: "2.4em",
+      lineHeight: "1.25",
+      fontWeight: "700",
+    },
+    lg: {
+      fontSize: "4em",
+      lineHeight: "1.1",
+      fontWeight: "700",
+    },
+    xl: {
+      fontSize: "6em",
+      lineHeight: "1.1",
+      fontWeight: "700",
+    },
   },
-  variants: {
-    light: {},
-    gray: {},
-    dark: {},
-  },
-  // The default size and variant values
+
+  // Default size
   defaultProps: {
-    size: "regular",
-    variant: "dark",
+    size: "md",
   },
 });
