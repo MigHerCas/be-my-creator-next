b01a41c285ca830416bb732363cee667eb28af51
Miguel Hernanz <migherdcas@gmail.com>
2022-12-22 19:03:48 +0800

    simplify Layout

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -10,15 +10,13 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
-      <Box margin="8">
-        <Header />
-        <Box as="main" marginY={22}>
-          {children}
-        </Box>
-        <Footer />
+    <>
+      <Header />
+      <Box as="main" marginY={22}>
+        {children}
       </Box>
-    </Box>
+      <Footer />
+    </>
   );
 };
 
