d195e8b88453bf6ab1d603958ef1bc77c1b8de44
Miguel Hernanz <migherdcas@gmail.com>
2022-12-22 21:46:03 +0800

    add header component

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,4 +1,6 @@
-import { Box, Flex } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { Flex, Spacer, HStack, Link } from "@chakra-ui/react";
+import CTA from "@components/cta/CTA";
 import Logo from "@components/logo/Logo";
 
 import ThemeToggle from "@components/themeToggle/ThemeToggle";
@@ -6,12 +8,44 @@ import { MAX_WIDTH } from "@helpers/ui-values";
 
 const Header = () => {
   return (
-    <Flex as="header" maxW={MAX_WIDTH} mx="auto" width="full" align="center">
+    <Flex
+      as="header"
+      maxW={MAX_WIDTH}
+      px="30px"
+      mx="auto"
+      width="full"
+      align="center"
+    >
       <Logo />
+      <ThemeToggle />
+      <Spacer />
+      <Flex as="nav" role="navigation" gap={8}>
+        {/* Navigation links */}
+        <HStack as="ol" listStyleType="none">
+          <Link href="/" as={NextLink} p={4}>
+            Link 1
+          </Link>
+          <Link href="/" as={NextLink} p={4}>
+            Link 2
+          </Link>
+          <Link href="/" as={NextLink} p={4}>
+            Link 3
+          </Link>
+          <Link href="/" as={NextLink} p={4}>
+            Link 4
+          </Link>
+          <Link href="/styleguide" as={NextLink} p={4}>
+            Styleguide
+          </Link>
+        </HStack>
 
-      <Box marginLeft="auto">
-        <ThemeToggle />
-      </Box>
+        <Spacer />
+
+        {/* Main CTA */}
+        <HStack as="ol" listStyleType="none">
+          <CTA href="">Log in as a Brand</CTA>
+        </HStack>
+      </Flex>
     </Flex>
   );
 };
