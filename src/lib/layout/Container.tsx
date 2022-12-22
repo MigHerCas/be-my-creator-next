df754f6f3dcecb46f35325d62792b8f87fe9bd47
Miguel Hernanz <migherdcas@gmail.com>
2022-12-22 17:19:20 +0800

    htmlTag is now passed down via props on Container

--- src/lib/layout/Container.tsx
+++ src/lib/layout/Container.tsx
@@ -1,12 +1,13 @@
 import type { FC } from "react";
 import { Container as ChakraContainer } from "@chakra-ui/react";
-import type { StyleProps } from "@chakra-ui/react";
+import type { StyleProps, As } from "@chakra-ui/react";
 
 interface Props {
   children: React.ReactNode;
+  htmlTag: As;
 }
 
-const Container: FC<Props> = ({ children }) => {
+const Container: FC<Props> = ({ children, htmlTag = "div" }) => {
   const baseStyles: StyleProps = {
     position: "relative",
     maxWidth: "1380px",
@@ -16,7 +17,7 @@ const Container: FC<Props> = ({ children }) => {
   };
 
   return (
-    <ChakraContainer as="section" {...baseStyles}>
+    <ChakraContainer as={htmlTag} {...baseStyles}>
       {children}
     </ChakraContainer>
   );
