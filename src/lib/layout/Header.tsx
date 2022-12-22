7e4655904f088cf395085bc98fd9bca3ca37de8e
Miguel Hernanz <migherdcas@gmail.com>
2022-12-22 21:03:54 +0800

    move Logo to header component

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,10 +1,14 @@
 import { Box, Flex } from "@chakra-ui/react";
+import Logo from "@components/logo/Logo";
 
 import ThemeToggle from "@components/themeToggle/ThemeToggle";
+import { MAX_WIDTH } from "@helpers/ui-values";
 
 const Header = () => {
   return (
-    <Flex as="header" width="full" align="center">
+    <Flex as="header" maxW={MAX_WIDTH} mx="auto" width="full" align="center">
+      <Logo />
+
       <Box marginLeft="auto">
         <ThemeToggle />
       </Box>
--- src/lib/views/home/index.tsx
+++ src/lib/views/home/index.tsx
@@ -2,7 +2,6 @@ import { NextSeo } from "next-seo";
 import NextLink from "next/link";
 
 import { Container, Flex } from "@chakra-ui/react";
-import Logo from "@components/logo/Logo";
 import VideoModal from "@components/modal/VideoModal";
 import VideoPlayer from "@components/video/VideoPlayer";
 
@@ -11,8 +10,6 @@ const Home = () => {
     <Container>
       <Flex direction="column" gap={8}>
         <NextSeo title="Home" />
-
-        <Logo />
 
         <VideoModal>
           <VideoPlayer />
