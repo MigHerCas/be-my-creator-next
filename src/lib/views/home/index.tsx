e2c545cb40fe790f922b03c55804241343457c65
Miguel Hernanz <migherdcas@gmail.com>
2022-12-27 22:11:50 +0800

    import Stats section component inside Homepage

--- src/lib/views/home/index.tsx
+++ src/lib/views/home/index.tsx
@@ -1,12 +1,14 @@
 import { NextSeo } from "next-seo";
 
 import Hero from "./modules/Hero";
+import Stats from "./modules/Stats";
 
 const Home = () => {
   return (
     <>
       <NextSeo title="Home" />
       <Hero />
+      <Stats />
     </>
   );
 };
