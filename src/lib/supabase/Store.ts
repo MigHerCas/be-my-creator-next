732c43ef6e5a2806c51d5bac62099b835e753809
Miguel Hernanz <migherdcas@gmail.com>
2022-12-30 17:55:07 +0800

    create Store file to handle supabase

--- src/lib/supabase/Store.ts
+++ src/lib/supabase/Store.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Dispatch, SetStateAction } from "react";
+import { useEffect, useState } from "react";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
@@ -6,3 +8,62 @@ const SUPABASE_REFERENCE = process.env.NEXT_PUBLIC_SUPABASE_REFERENCE as string;
 
 export const API_REST_KEY = `https://${SUPABASE_REFERENCE}.supabase.co/rest/v1`;
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export type Lead = {
+  name: string;
+  email: string;
+  config: object;
+};
+
+/**
+ * Insert lead
+ */
+export const insertLead = async () => {
+  const newLead: Lead = {
+    name: "Andrea",
+    email: "andreacos@hotmail.com",
+    config: {},
+  };
+
+  try {
+    const { data } = await supabase.from("leads").insert([newLead]);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+/**
+ * Fetch all leads
+ * @param {function} setState Optionally pass in a hook or callback to set the state
+ */
+export const fetchLeads = async (
+  setState: Dispatch<SetStateAction<unknown>>
+) => {
+  console.log(supabase);
+  try {
+    const { data } = await supabase.from("leads").select("*");
+
+    if (setState) setState(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+/**
+ * @param {number} channelId the currently selected Channel
+ */
+export const useStore = () => {
+  const [leads, setLeads] = useState<unknown>([]);
+
+  // Load initial data and set up listeners
+  useEffect(() => {
+    // Get Channels
+    fetchLeads(setLeads);
+  }, []);
+
+  return {
+    leads,
+  };
+};
