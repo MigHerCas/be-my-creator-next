e83f0ad7f2643beefcb207b86c500dbe32adb645
Miguel Hernanz <migherdcas@gmail.com>
2023-01-03 10:53:15 +0800

    revert previous changes

--- src/lib/compositions/FixedBlobs.tsx
+++ src/lib/compositions/FixedBlobs.tsx
@@ -7,24 +7,22 @@ const FixedBlobs: FC = () => {
     <>
       <Box
         pos="fixed"
-        bottom={["500px", "400px", "300px", null, null, "30%"]}
-        left={["-150px", "-150px", "-150px", "-20%", null, "-10%"]}
+        bottom={["50%", "45%", "35%", null, null, "30%"]}
+        left={["-40%", "-30%", "-20%", "-20%", null, "-10%"]}
         w={["300px", "350px", "400px", null, "500px", "600px"]}
         h={["300px", "350px", "400px", null, "500px", "600px"]}
         zIndex="-1"
-        transition="all 350ms ease"
       >
         <Blob type={2} />
       </Box>
 
       <Box
         pos="fixed"
-        top={["400px", null, "50%"]}
-        right={["-150px", "-200px", "-20%", "-10%"]}
+        top={["50%", null, "50%"]}
+        right={["-50%", "-35%", "-20%", "-10%"]}
         w={["300px", "400px", "400px", null, "500px", "600px"]}
         h={["300px", "400px", "400px", null, "500px", "600px"]}
         zIndex="-1"
-        transition="all 350ms ease"
       >
         <Blob type={0} isInverted />
       </Box>
