1d0e64ae7eb72614c1e1a7eecc42c1607269f0ef
Miguel <migherdcas@gmail.com>
2023-01-03 11:25:37 +0800

    Add transition to blobs re-positioning (#33)
    * add transition to blobs
    
    * add meta tag to fix the issue
    
    * try px instead of %
    
    * revert previous changes
    
    * add freezing transition
    
    * add fixed wrapper
    
    * add more fixes
    
    * and more fixes
    
    * remove fixed body
    
    * try use100vh component

--- src/lib/compositions/FixedBlobs.tsx
+++ src/lib/compositions/FixedBlobs.tsx
@@ -1,17 +1,26 @@
 import { Box } from "@chakra-ui/react";
 import Blob from "@components/blob/Blob";
 import type { FC } from "react";
+import { use100vh } from "react-div-100vh";
 
 const FixedBlobs: FC = () => {
+  const height = use100vh();
+
   return (
-    <>
+    <Box
+      pos="fixed"
+      h={height || "100vh"}
+      w="100vw"
+      top="0"
+      left="0"
+      zIndex="-1"
+    >
       <Box
         pos="fixed"
         bottom={["50%", "45%", "35%", null, null, "30%"]}
         left={["-40%", "-30%", "-20%", "-20%", null, "-10%"]}
         w={["300px", "350px", "400px", null, "500px", "600px"]}
         h={["300px", "350px", "400px", null, "500px", "600px"]}
-        zIndex="-1"
       >
         <Blob type={2} />
       </Box>
@@ -22,11 +31,10 @@ const FixedBlobs: FC = () => {
         right={["-50%", "-35%", "-20%", "-10%"]}
         w={["300px", "400px", "400px", null, "500px", "600px"]}
         h={["300px", "400px", "400px", null, "500px", "600px"]}
-        zIndex="-1"
       >
         <Blob type={0} isInverted />
       </Box>
-    </>
+    </Box>
   );
 };
 
