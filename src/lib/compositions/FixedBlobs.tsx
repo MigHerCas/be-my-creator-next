95c4dfedbb7fbdee75d122d0b6d09d1a606126bf
Miguel Hernanz <migherdcas@gmail.com>
2023-01-03 10:46:13 +0800

    try px instead of %

--- src/lib/compositions/FixedBlobs.tsx
+++ src/lib/compositions/FixedBlobs.tsx
@@ -7,8 +7,8 @@ const FixedBlobs: FC = () => {
     <>
       <Box
         pos="fixed"
-        bottom={["50%", "45%", "35%", null, null, "30%"]}
-        left={["-40%", "-30%", "-20%", "-20%", null, "-10%"]}
+        bottom={["500px", "400px", "300px", null, null, "30%"]}
+        left={["-150px", "-150px", "-150px", "-20%", null, "-10%"]}
         w={["300px", "350px", "400px", null, "500px", "600px"]}
         h={["300px", "350px", "400px", null, "500px", "600px"]}
         zIndex="-1"
@@ -19,8 +19,8 @@ const FixedBlobs: FC = () => {
 
       <Box
         pos="fixed"
-        top={["50%", null, "50%"]}
-        right={["-50%", "-35%", "-20%", "-10%"]}
+        top={["400px", null, "50%"]}
+        right={["-150px", "-200px", "-20%", "-10%"]}
         w={["300px", "400px", "400px", null, "500px", "600px"]}
         h={["300px", "400px", "400px", null, "500px", "600px"]}
         zIndex="-1"
