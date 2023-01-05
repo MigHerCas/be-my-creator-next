14ea6a78714566e5c7bb6748d085af2090b8002e
Miguel Hernanz <migherdcas@gmail.com>
2023-01-05 17:27:26 +0800

    create RadioGroupStack component

---
+++ src/lib/views/new-project/modules/RadioGroupStack.tsx
@@ -0,0 +1,52 @@
+import {
+  FormControl,
+  FormLabel,
+  HStack,
+  useRadioGroup,
+} from "@chakra-ui/react";
+import RadioCard from "@components/radioCard/RadioCard";
+import type { FC } from "react";
+
+interface Props {
+  options: Array<string>;
+  label: string;
+  name: string;
+  defaultValue: string;
+  onChange: () => void;
+}
+const RadioGroupStack: FC<Props> = ({
+  label,
+  options,
+  defaultValue,
+  name,
+  onChange,
+}) => {
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name,
+    defaultValue,
+    onChange,
+  });
+
+  const group = getRootProps();
+
+  return (
+    <FormControl w="60%" mx="auto">
+      <FormLabel fontSize="24px" fontWeight="400" mb="30px">
+        {label}
+      </FormLabel>
+
+      <HStack {...group}>
+        {options.map((value) => {
+          const radio = getRadioProps({ value });
+          return (
+            <RadioCard key={value} {...radio}>
+              {value}
+            </RadioCard>
+          );
+        })}
+      </HStack>
+    </FormControl>
+  );
+};
+
+export default RadioGroupStack;
