cec312e83e5380ce4aeec29f2d923bb2d37018a4
Miguel Hernanz <migherdcas@gmail.com>
2023-01-05 17:54:16 +0800

    implement NewProject view basic behaviour and logic

--- src/lib/views/new-project/index.tsx
+++ src/lib/views/new-project/index.tsx
@@ -1,63 +1,98 @@
-import {
-  Flex,
-  Heading,
-  HStack,
-  Input,
-  InputGroup,
-  InputRightElement,
-  useRadioGroup,
-} from "@chakra-ui/react";
-import RadioCard from "@components/radioCard/RadioCard";
-import type { NextPage } from "next";
+import { Button, Flex, Heading, useColorMode } from "@chakra-ui/react";
+import Layout from "@layout/index";
+import type { NextPageWithLayout } from "@pages/_app";
 import { NextSeo } from "next-seo";
-import { Check } from "react-feather";
+import type { ReactElement } from "react";
+import { useEffect } from "react";
+import type { SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
-const NewProjectView: NextPage = () => {
-  const options = ["react", "vue", "svelte"];
+import InputGroup from "./modules/InputGroup";
+import RadioGroupStack from "./modules/RadioGroupStack";
 
-  const { getRootProps, getRadioProps } = useRadioGroup({
-    name: "framework",
-    defaultValue: "react",
-    onChange: console.log,
-  });
+type Inputs = {
+  email: string;
+  brandName: string;
+  platforms: ["instagram", "tiktok", "twitter"];
+  teamSize: ["1-5", "10-20", "+20"];
+};
 
-  const group = getRootProps();
+const NewProjectView: NextPageWithLayout = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    if (colorMode === "light") toggleColorMode();
+  }, [colorMode, toggleColorMode]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  const brandNameField = register("email", { required: true });
+  const emailField = register("brandName");
 
   return (
     <>
-      <NextSeo title="Home" />
-      <Flex flexDir="column" gap="30px" p="60px" bgColor="white">
+      <NextSeo title="New project" />
+      <Flex
+        as="form"
+        onSubmit={handleSubmit(onSubmit)}
+        flexDir="column"
+        alignItems="center"
+        gap="60px"
+      >
         <Heading size="lg" textAlign="center">
           New project!
         </Heading>
-        <InputGroup alignItems="center" display="flex">
-          <Input
-            placeholder="Input here"
-            size="lg"
-            height="65px"
-            minW="16em"
-            py="18px"
-            px="16px"
-            borderRadius="8px"
-          />
-          <InputRightElement>
-            <Check stroke="#00C4A2" />
-          </InputRightElement>
-        </InputGroup>
 
-        <HStack {...group}>
-          {options.map((value) => {
-            const radio = getRadioProps({ value });
-            return (
-              <RadioCard key={value} {...radio}>
-                {value}
-              </RadioCard>
-            );
-          })}
-        </HStack>
+        <RadioGroupStack
+          key="platform"
+          name="platform"
+          options={["instagram", "tiktok", "twitter"]}
+          defaultValue="tiktok"
+          label="Platform"
+          onChange={console.log}
+        />
+
+        <RadioGroupStack
+          key="team"
+          name="team"
+          options={["1-5", "10-20", "+20"]}
+          defaultValue="1-5"
+          label="Team"
+          onChange={console.log}
+        />
+
+        <InputGroup
+          label="Let's start with the name of your brand"
+          type="text"
+          placeholder="Your name brand"
+          registerCallback={brandNameField}
+        />
+        {errors.email && <span>This field is required</span>}
+
+        <InputGroup
+          label="Which email could we use to contact you?"
+          type="email"
+          placeholder="name@example.com"
+          helperText="We'll never share your email."
+          registerCallback={emailField}
+        />
+
+        <Button type="submit" size="lg">
+          Submit
+        </Button>
       </Flex>
     </>
   );
+};
+
+NewProjectView.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
 };
 
 export default NewProjectView;
