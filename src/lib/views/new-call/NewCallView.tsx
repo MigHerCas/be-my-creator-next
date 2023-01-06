0ef71672e38fc17e1f897144ea417837bfb93423
Miguel Hernanz <migherdcas@gmail.com>
2023-01-06 17:03:29 +0800

    style wrapper on NewCall

--- src/lib/views/new-call/NewCallView.tsx
+++ src/lib/views/new-call/NewCallView.tsx
@@ -1,10 +1,11 @@
-import { Flex, Heading, Text, useColorMode } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, useColorMode } from "@chakra-ui/react";
 import CalendlyIntegration from "@compositions/CalendlyIntegration";
 import Layout from "@layout/index";
 import type { NextPageWithLayout } from "@pages/_app";
 import { NextSeo } from "next-seo";
 import type { ReactElement } from "react";
 import { useEffect } from "react";
+import { Video } from "react-feather";
 
 const NewCallView: NextPageWithLayout = () => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -16,10 +17,27 @@ const NewCallView: NextPageWithLayout = () => {
   return (
     <>
       <NextSeo title="New project" />
-      <Flex flexDir="column" alignItems="center" gap="30px">
-        <Heading size="lg" textAlign="center" mb="0">
-          Book a call with us
-        </Heading>
+      <Flex
+        flexDir="column"
+        alignItems="center"
+        gap="30px"
+        py="60px"
+        className="glassmorphic--light"
+      >
+        <Flex alignItems="center" gap={8}>
+          <Box
+            p={5}
+            display="inline-block"
+            bgColor="#272727"
+            borderRadius="full"
+          >
+            <Video height="40px" width="40px" stroke="#BCE500" />
+          </Box>
+          <Heading size="lg" textAlign="center" mb={0}>
+            Book a call with us
+          </Heading>
+        </Flex>
+
         <Text>
           Let&apos;s get started with a simple call to see how we can help each
           other.
