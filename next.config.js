fa17b5ac980395d7fe4dbc150b942b9c08303e14
Miguel Hernanz <migherdcas@gmail.com>
2023-01-11 13:09:16 +0800

    add sample image hostname for testing purposes

--- next.config.js
+++ next.config.js
@@ -20,5 +20,14 @@ module.exports = withPWA({
     NEXT_PUBLIC_SUPABASE_REFERENCE: process.env.NEXT_PUBLIC_SUPABASE_REFERENCE,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY
-  }
+  },
+  // TODO: Remove this before production
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd24ovhgu8s7341.cloudfront.net',
+      },
+    ],
+  },
 });
