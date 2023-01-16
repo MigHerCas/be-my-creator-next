edddb5940b61cd847cc795dfaf3a9e8d00b06064
Miguel Hernanz <migherdcas@gmail.com>
2023-01-16 20:53:18 +0800

    add left and right FAQ items

--- src/lib/compositions/FAQ/FAQ.tsx
+++ src/lib/compositions/FAQ/FAQ.tsx
@@ -4,6 +4,7 @@ import {
   AccordionItem,
   AccordionPanel,
   Box,
+  Flex,
   Heading,
   IconButton,
   Tab,
@@ -23,7 +24,13 @@ const FAQ: FC = () => {
       <Heading mx="auto" maxW="20ch" size="md" textAlign="center" mb="30px">
         Any question?
       </Heading>
-      <Tabs variant="solid-rounded" isFitted isLazy justifyContent="center">
+      <Tabs
+        variant="solid-rounded"
+        isFitted
+        isLazy
+        justifyContent="center"
+        mb="60px"
+      >
         <TabList
           p="10px"
           border="1px solid"
@@ -49,75 +56,83 @@ const FAQ: FC = () => {
         </TabList>
 
         <TabPanels>
-          {FAQ_CONTENT.map(({ id, items }) => (
+          {FAQ_CONTENT.map(({ id, leftItems, rightItems }) => (
             <TabPanel p={0} key={id}>
               <Accordion
                 allowMultiple
-                allowToggle
                 display="grid"
                 gridTemplateColumns={["1fr", null, null, "1fr 1fr"]}
                 gap="30px"
                 alignItems="flex-start"
               >
-                {items.map(({ title, content }) => {
-                  return (
-                    <AccordionItem
-                      borderWidth="1px"
-                      borderStyle="solid"
-                      borderColor="gray.200"
-                      borderRadius="20px"
-                      backgroundColor="white"
-                      cursor="pointer"
-                    >
-                      {({ isExpanded }) => (
-                        <>
-                          <Box>
-                            <AccordionButton
-                              _hover={{ bgColor: "inherit" }}
-                              p="30px"
-                              as="div"
-                              flexWrap="nowrap"
-                            >
-                              <Heading
-                                size="sm"
-                                as="h4"
-                                flex="1"
-                                textAlign="left"
-                                fontSize="1.3125rem"
-                                mb={0}
-                              >
-                                {title}
-                              </Heading>
+                {[leftItems, rightItems].map((items, index) => (
+                  <Flex
+                    key={`${id}-${index === 0 ? "left" : "right"}`}
+                    flexDir="column"
+                    gap="30px"
+                  >
+                    {items.map(({ title, content }) => {
+                      return (
+                        <AccordionItem
+                          key={title}
+                          borderWidth="1px"
+                          borderStyle="solid"
+                          borderColor="gray.200"
+                          borderRadius="20px"
+                          backgroundColor="white"
+                          cursor="pointer"
+                        >
+                          {({ isExpanded }) => (
+                            <>
+                              <Box>
+                                <AccordionButton
+                                  _hover={{ bgColor: "inherit" }}
+                                  p="30px"
+                                  as="div"
+                                  flexWrap="nowrap"
+                                >
+                                  <Heading
+                                    size="sm"
+                                    as="h4"
+                                    flex="1"
+                                    textAlign="left"
+                                    fontSize="1.3125rem"
+                                    mb={0}
+                                  >
+                                    {title}
+                                  </Heading>
 
-                              <IconButton
-                                boxSize="40px"
-                                aria-label="theme toggle"
-                                bgColor="gray.800"
-                                borderRadius="full"
-                                _hover={{ bgColor: "gray.800" }}
-                                _active={{ bgColor: "gray.800" }}
+                                  <IconButton
+                                    boxSize="40px"
+                                    aria-label="theme toggle"
+                                    bgColor="gray.800"
+                                    borderRadius="full"
+                                    _hover={{ bgColor: "gray.800" }}
+                                    _active={{ bgColor: "gray.800" }}
+                                  >
+                                    {isExpanded ? (
+                                      <Minus stroke="white" />
+                                    ) : (
+                                      <Plus stroke="white" />
+                                    )}
+                                  </IconButton>
+                                </AccordionButton>
+                              </Box>
+                              <AccordionPanel
+                                px="30px"
+                                pb="30px"
+                                pt={0}
+                                fontSize="1.125rem"
                               >
-                                {isExpanded ? (
-                                  <Minus stroke="white" />
-                                ) : (
-                                  <Plus stroke="white" />
-                                )}
-                              </IconButton>
-                            </AccordionButton>
-                          </Box>
-                          <AccordionPanel
-                            px="30px"
-                            pb="30px"
-                            pt={0}
-                            fontSize="1.125rem"
-                          >
-                            {content}
-                          </AccordionPanel>
-                        </>
-                      )}
-                    </AccordionItem>
-                  );
-                })}
+                                {content}
+                              </AccordionPanel>
+                            </>
+                          )}
+                        </AccordionItem>
+                      );
+                    })}
+                  </Flex>
+                ))}
               </Accordion>
             </TabPanel>
           ))}
