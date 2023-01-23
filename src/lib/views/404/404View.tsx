1e6cddde9c8e4b5ce5a0e82ff243a43f2c62ea59
Miguel Hernanz <migherdcas@gmail.com>
2023-01-23 14:39:23 +0800

    refactor 404 view to improve image

--- src/lib/views/404/404View.tsx
+++ src/lib/views/404/404View.tsx
@@ -1,26 +1,23 @@
-import {
-  Box,
-  Button,
-  Heading,
-  Image,
-  Text,
-  useColorMode,
-  Flex,
-} from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import CTA from "@components/cta/CTA";
 import { NextSeo } from "next-seo";
-import Link from "next/link";
+import NextImage from "next/image";
 import type { FC } from "react";
 
 const Page404View: FC = () => {
-  const { colorMode } = useColorMode();
-
   return (
-    <Flex minHeight="60vh" direction="column" justifyContent="center">
+    <Flex
+      minHeight="calc(100vh - 90px)"
+      direction="column"
+      justifyContent="center"
+    >
       <NextSeo title="404 Not Found" />
-      {/* TODO: add floating animation */}
       <Box width={{ base: "100%", sm: "70%", md: "50%" }} margin="0 auto">
-        <Image
+        <NextImage
           src="/404 Error-pana.svg"
+          width={600}
+          height={400}
+          priority
           alt="Error 404 not found Illustration"
         />
       </Box>
@@ -33,14 +30,15 @@ const Page404View: FC = () => {
           <Text fontSize="sm" color="gray">
             It&apos;s Okay!
           </Text>
-          <Button
-            as={Link}
+
+          <CTA
             href="/"
-            backgroundColor={colorMode === "light" ? "gray.300" : "teal.500"}
-            size="sm"
+            variant="primary"
+            icon="arrow"
+            customStyles={{ mx: "auto" }}
           >
             Let&apos;s Head Back
-          </Button>
+          </CTA>
         </Box>
       </Box>
     </Flex>
