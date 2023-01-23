344a6513d2515c08639f083dd3883d52d8a36b39
Miguel Hernanz <migherdcas@gmail.com>
2023-01-23 13:34:07 +0800

    remove fixed position from VideoModalDrawer and adjusted layout

--- src/lib/components/video/VideoModalDrawer.tsx
+++ src/lib/components/video/VideoModalDrawer.tsx
@@ -1,4 +1,5 @@
-import { Box, Divider, Fade, Heading, Text } from "@chakra-ui/react";
+import { Box, Divider, Fade, Flex, Heading, Text } from "@chakra-ui/react";
+import CTA from "@components/cta/CTA";
 import type { FC } from "react";
 
 interface Props {
@@ -9,40 +10,41 @@ const VideoModalDrawer: FC<Props> = ({ isOpen }) => {
   return (
     <Fade in={isOpen}>
       <Box
-        pos="fixed"
-        top="50%"
-        right={0}
         width="500px"
+        flexShrink={0}
         height="100vh"
-        display={{ base: "none", "2xl": "flex" }}
-        flexDir="column"
-        justifyContent="flex-start"
-        gap="30px"
+        display={{ base: "none", "2xl": "block" }}
         bgColor="white"
-        transform="translateY(-50%)"
         zIndex={4}
-        p="60px"
+        p="60px 30px 30px 60px"
         borderLeft="30px solid #00C4A2"
       >
-        <Box>
-          <Heading size="md">Create your first campaign</Heading>
-          <Text size="md" mb="30px">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-            distinctio ducimus amet possimus non, voluptatibus nobis alias
-            maxime.
-          </Text>
-        </Box>
+        <Flex justifyContent="space-between" flexDir="column" gap="30px">
+          <Box>
+            <Heading size="md">Create your first campaign</Heading>
+            <Text size="md" mb="30px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              distinctio ducimus amet possimus non, voluptatibus nobis alias
+              maxime.
+            </Text>
+            <CTA href="/new-project" variant="primary" icon="plus" showDot>
+              Start now
+            </CTA>
+          </Box>
 
-        <Divider />
-
-        <Box>
-          <Heading size="md">Meet us</Heading>
-          <Text size="md" mb="30px">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-            distinctio ducimus amet possimus non, voluptatibus nobis alias
-            maxime.
-          </Text>
-        </Box>
+          <Divider />
+          <Box>
+            <Heading size="md">Meet us</Heading>
+            <Text size="md" mb="30px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              distinctio ducimus amet possimus non, voluptatibus nobis alias
+              maxime.
+            </Text>
+            <CTA href="/new-project" variant="secondary" icon="arrow">
+              Book a call
+            </CTA>
+          </Box>
+        </Flex>
       </Box>
     </Fade>
   );
