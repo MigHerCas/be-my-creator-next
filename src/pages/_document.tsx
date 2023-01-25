58016bd8e8ce7331fc0167c06790286323113bf6
Miguel Hernanz <migherdcas@gmail.com>
2023-01-25 13:20:32 +0800

    add nprogress css link tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,8 +14,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap"
             rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+            integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
           />
           <Meta />
         </Head>
