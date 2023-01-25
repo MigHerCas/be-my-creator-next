9c6a1bfbc1b474171429e7838c63f6355f4c30a0
Miguel Hernanz <migherdcas@gmail.com>
2023-01-25 14:17:27 +0800

    add theme colors inside ui-values

--- src/lib/helpers/ui-values.ts
+++ src/lib/helpers/ui-values.ts
@@ -4,10 +4,21 @@ const SLIDER_CARD_WIDTH_SIZES = ["242px", null, "315px", null, null, "360px"];
 const SLIDER_CARD_HEIGHT_SIZES = ["432px", null, "560px", null, null, "640px"];
 const SECTION_VERTICAL_SPACING = ["30px", null, "40px", null, "60px"];
 
+// Theme colors
+const themeColors = {
+  black: "#1c1c1c",
+  darkGrey: "#272727",
+  ligthGrey: "hsla(0, 0%, 100%, 0.5)",
+  primary: "#00C4A2",
+  orange: "#FCC54E",
+  pistache: "#BCE500",
+};
+
 export {
   APP_MAX_WIDTH,
   HEADER_HEIGHT,
   SLIDER_CARD_WIDTH_SIZES,
   SLIDER_CARD_HEIGHT_SIZES,
+  themeColors,
   SECTION_VERTICAL_SPACING,
 };
