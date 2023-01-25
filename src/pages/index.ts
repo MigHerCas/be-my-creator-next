4f512be3959e33a3c1646f1073e5b9424302332f
Miguel Hernanz <migherdcas@gmail.com>
2023-01-25 15:25:49 +0800

    try stale-while-revalidate cache option

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,20 @@
 import HomeView from "@views/home/HomeView";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export async function getServerSideProps({
+  res,
+}: {
+  req: NextApiRequest;
+  res: NextApiResponse;
+}) {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+
+  return {
+    props: {},
+  };
+}
 
 export default HomeView;
