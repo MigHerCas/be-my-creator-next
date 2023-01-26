39935e91ba31359a79a3e8b6309e87bd1c4f32aa
Miguel Hernanz <migherdcas@gmail.com>
2023-01-26 12:41:02 +0800

    create FormCustomCheckbox component

---
+++ src/lib/views/new-project/modules/FormCustomCheckbox.tsx
@@ -0,0 +1,39 @@
+import type { UseCheckboxProps } from "@chakra-ui/react";
+import { useCheckbox, Box } from "@chakra-ui/react";
+import { themeColors } from "@helpers/ui-values";
+import type { PropsWithChildren } from "react";
+import { forwardRef } from "react";
+
+const FormCustomCheckbox = forwardRef<
+  HTMLInputElement,
+  PropsWithChildren<UseCheckboxProps>
+>(({ children, ...checkboxProps }, ref) => {
+  const { state, getCheckboxProps, getInputProps } = useCheckbox(checkboxProps);
+
+  const input = getInputProps({ ref });
+  const checkbox = getCheckboxProps();
+  return (
+    <Box as="label" w="full">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        color="white"
+        bgColor={state.isChecked ? themeColors.primary : themeColors.black}
+        borderRadius="md"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        boxShadow={state.isFocused ? "outline" : "none"}
+        flex="1"
+        minH="100px"
+        fontSize="20px"
+        fontWeight={500}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+});
+
+export default FormCustomCheckbox;
