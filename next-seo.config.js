2177aae3884c034b4d5ed5dd3579b56631cba36d
Miguel <migherdcas@gmail.com>
2023-01-29 17:31:25 +0800

    SEO improvements (#83)
    * next-seo config file
    
    * update next-seo config file

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,24 +1,24 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nextarter-chakra",
-  titleTemplate: "%s | nextarter-chakra",
+  title: "Be My Creator",
+  titleTemplate: "%s | UGC",
   defaultTitle: "nextarter-chakra",
-  description: "Next.js + chakra-ui + TypeScript template",
-  canonical: "https://nextarter-chakra.sznm.dev",
+  description: "Your UGC agency",
+  canonical: "https://bemycreator.com",
   openGraph: {
-    url: "https://nextarter-chakra.sznm.dev",
-    title: "nextarter-chakra",
-    description: "Next.js + chakra-ui + TypeScript template",
+    url: "https://bemycreator.com",
+    title: "Be My Creator | UGC ",
+    description: "Your UGC agency for brands and creators",
     images: [
       {
-        url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
-        alt: "nextarter-chakra.sznm.dev og-image",
+        url: "https://xudvhxwolsxldijaphhq.supabase.co/storage/v1/object/sign/main-bucket/images/video-capture-9275.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtYWluLWJ1Y2tldC9pbWFnZXMvdmlkZW8tY2FwdHVyZS05Mjc1LnBuZyIsImlhdCI6MTY3NDk4Mzc1MiwiZXhwIjoxNzA2NTE5NzUyfQ.9v6-eHALF8ilenK7N7OXOJmckPSxk-xcWQsV1S93Fs4&t=2023-01-29T09%3A15%3A52.980Z",
+        alt: "bemycreator.com og-image",
       },
     ],
-    site_name: "nextarter-chakra",
+    site_name: "BeMyCreator",
   },
   twitter: {
-    handle: "@sozonome",
+    handle: "@bemycreator",
     cardType: "summary_large_image",
   },
 };
