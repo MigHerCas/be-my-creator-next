d0b89c4d691aa30aa43e572547d0599da1fe0248
Miguel Hernanz <migherdcas@gmail.com>
2023-02-17 17:23:14 +0800

    wrong url added to background image

--- src/lib/views/home/modules/Hero/HeroPlayer.tsx
+++ src/lib/views/home/modules/Hero/HeroPlayer.tsx
@@ -26,7 +26,7 @@ const HeroPlayer: FC<Props> = ({ source }) => {
   const beforePseudoElementStyling = {
     content: '""',
     backgroundImage:
-      "url(https://xudvhxwolsxldijaphhq.supabase.co/storage/v1/object/sign/main-bucket/videos/629a1b846dd7d3528640ea03_How%20To%20Example%20Home-transcode%20(1).mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtYWluLWJ1Y2tldC92aWRlb3MvNjI5YTFiODQ2ZGQ3ZDM1Mjg2NDBlYTAzX0hvdyBUbyBFeGFtcGxlIEhvbWUtdHJhbnNjb2RlICgxKS5tcDQiLCJpYXQiOjE2NzY2MjUzMTgsImV4cCI6MTcwODE2MTMxOH0.TAneDUGLXgLnWA-BLFucWtS2v5eYVpVca_mGSbJI4ic&t=2023-02-17T09%3A15%3A18.151Z)",
+      "url(https://xudvhxwolsxldijaphhq.supabase.co/storage/v1/object/sign/main-bucket/images/video-capture-2381.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtYWluLWJ1Y2tldC9pbWFnZXMvdmlkZW8tY2FwdHVyZS0yMzgxLnBuZyIsImlhdCI6MTY3NjYyNTc1OCwiZXhwIjoxNzA4MTYxNzU4fQ.a37MQNxxlQaOsrx_hbxHup3Fa5c9QHbkuu6fOYHpnKI&t=2023-02-17T09%3A22%3A38.457Z)",
     width: ["264px", null, null, "310px"],
     height: ["545", null, null, "640px"],
     backgroundRepeat: "no-repeat",
